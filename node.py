efbad66947b1b99ae7f8d96539ab9b7b8f62073e
Reshma Sreekumar <reshmaisat@gmail.com>
2015-11-26 11:41:24 +0530

    getbalance algo3

--- node.py
+++ node.py
@@ -15,49 +15,71 @@ class node:
 		self.genesis=NULL
 		
 
-#Algorithm:
+'''
+Algorithm:
 		
-#1. proofofwork	( new_block )
-#2. ver = verify_nonce ( prop_block )
-#3. maxheight = traverse to the end of a leaf list, check max height.
-#4.	get the block with max height 
-#5. if ver == 1: (i.e, if the nonce is verified by the node)
-#6. 	if node.blockhead.height == maxheight:
-#7.			prop_block should point to node.blockhead
-#8. 		node.blockhead=prop_block
-#9.		else
-#10.			 node.blockhead = blockmaxheight
-#11.			 prop_block should point to node.blockhead
-#12. 			node.blockhead=prop_block
+1. proofofwork	( new_block )
+2. ver = verify_nonce ( prop_block )
+3. maxheight = traverse to the end of a leaf list, check max height.
+4.	get the block with max height 
+5. if ver == 1: (i.e, if the nonce is verified by the node)
+6. 	if node.blockhead.height == maxheight:
+7.			prop_block should point to node.blockhead
+8. 		node.blockhead=prop_block
+9.		else
+10.			 node.blockhead = blockmaxheight
+11.			 prop_block should point to node.blockhead
+12. 			node.blockhead=prop_block
 	
+'''
+'''
+On receiving a block proposed by another node:
+1. Verify the POW 
+2. If the block is valid
+	a. block.prev_hash=blockhead
+	   bloackhead=block
+   else
+   		do nothing 
+   		 
+'''
 
-	'''
-	On receiving a block proposed by another node:
-	1. Verify the POW 
-	2. If the block is valid
-		a. block.prev_hash=blockhead
-		   bloackhead=block
-	   else
-	   		do nothing 
-	   		 
-	'''
+
+'''
+To find the balance in our wallet
+
+getbalance()
+1.Traverse the consesus chain
+2.balance = 0
+3.For each block in the chain
+	a.For each transaction from bottom to top
+		i.If it's a transaction created by self
+			i1.balance = balance + value of output transaction destined to self.publickey
+			i2.break loop
+		ii.Else
+			ii1.If it contains transaction destined to self.publickey
+				balance = balance + value of its output transaction
+				
+4. Return balance
+
+'''	
 	
 	
-	'''
-	To find the balance in our wallet
+getbalance(self):
+	blockptr = blockhead
+	balance = 0
+	while blockptr != self.genesis :
+		for i in range(blockptr.max_trans_num,0,-1) :
+			if ( blockptr.translist[i].sign == node.sign ) :
+				for j in range(blockptr.translist[i].outcount) :
+					 if ( blockptr.translist[i].outlist[j].addr == self.publickey ) :
+					 	balance = balance + blockptr.translist[i].outlist[j].value
+					 	break
+			else : 
+				for j in range(blockptr.translist[i].outcount) :
+					if ( blockptr.translist[i].outlist[j].addr == self.publickey ) :
+						balance = balance + blockptr.translist[i].outlist[j].value
+						
+						
+	return balance
 	
-	getbalance()
-	1.Traverse the consesus chain
-	2.balance = 0
-	3.For each block in the chain
-		a.For each transaction from bottom to top
-			i.If it's a transaction created by self
-				i1.balance = balance + value of output transaction destined to self.publickey
-				i2.break loop
-			ii.Else
-				ii1.If it contains transaction destined to self.publickey
-					balance = balance + value of its output transaction
-					
-	4. Return balance
 	
-	'''		
