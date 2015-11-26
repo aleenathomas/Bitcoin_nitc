b43bb88c9f1f67ca52b1d33be493f723c60f257e
reshmat <reshmathomas1194@gmail.com>
2015-11-26 13:49:09 +0530

    Modified node.py

--- node.py
+++ node.py
@@ -1,12 +1,9 @@
 #\xhh 	Character with hex value hh
 
 from block import *
-<<<<<<< HEAD
 from proof_of_work import *
 from blockchain import *
-=======
-from pow import *
->>>>>>> b15a3407dcf4c5e8741ecdf0596b8d4729a865f4
+
 
 class node:
 	def __init__(self):
@@ -17,7 +14,7 @@ class node:
 		self.maxnumtrans = 1000
 		self.database = [[0] * 2 for i in xrange(maxnumtrans)]
 		self.top = 0
-		self.genesis=NULL
+		self.genesis = None
 		
 
 
