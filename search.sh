48def517ba952e05571db64d4bbe01624c66c42c
aleenathomas <aleenathomas203@gmail.com>
2015-11-28 14:35:57 +0530

    verified : validate trans

--- search.sh
+++ search.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-#while :
-#do
-#timeout gnunet-search -N 1 "$1" > filestodownload.sh
-timeout 100s gnunet-search "$1" > filestodownload.sh
-gnunet-search "$1" > filestodownload.sh
+timeout 10s gnunet-search "$1" > filestodownload.sh
 grep -v '^#' filestodownload.sh > download.sh
 rm filestodownload.sh
 chmod +x download.sh
 ./download.sh
-#mv $1 $i
-#let i=i+1
-#done
-
