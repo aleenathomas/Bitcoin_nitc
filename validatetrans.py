8c0eda4f960a1bd043f07b00b82e9372d296867d
aleenathomas <aleenathomas203@gmail.com>
2015-11-28 15:00:58 +0530

    testing start action

--- validatetrans.py
+++ validatetrans.py
@@ -6,4 +6,5 @@ result = t.validatetrans(createnode.i_am)
 if result == True:
 	#add transaction to the current block maintained by the node
 	createnode.i_am.currentblock.add_trans_to_block(t)
-	block.blocktofile(createnode.i_am.currentblock, "the contents")	
+	print createnode.i_am.currentblock.prev_hash	
+#	block.blocktofile(createnode.i_am.currentblock, "the contents")	
